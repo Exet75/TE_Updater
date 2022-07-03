6ea10909421b08e7691936bf87f176b6d2d074c1
Exet <89310199+Exet75@users.noreply.github.com>
2022-07-03 10:09:21 +0300

    Delete TE_updater directory

--- TE_updater/Setup/completed.sh
+++
@@ -1,6 +0,0 @@
-echo ''
-echo '--- INSTALLATIIONS IS COMPLETED! ---'
-echo ''
-
-echo '|SUPER-CONFIGURATRION-STARTED|'
-bash Super_conf.sh
--- TE_updater/Setup/no_root.sh
+++
@@ -1,2 +0,0 @@
-pkg update && pkg upgrade && pkg install git python2 python3 php perl ruby hping3 micro metasploit openssl-tool openssh wget curl  ranger && git clone https://github.com/kuburan/txtool && cd txtool && chmod +x install.py && ./install.py 
-bash completed.sh  
